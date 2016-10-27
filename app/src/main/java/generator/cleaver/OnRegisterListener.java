a53336217a8237b7de87b9f11e8dfbffebffe2ff
P SATHISH KUMAR <sathishp.railway@gmail.com>
2016-10-27 17:18:42 +0530

    Update OnRegisterListener.java

--- app/src/main/java/generator/cleaver/OnRegisterListener.java
+++ app/src/main/java/generator/cleaver/OnRegisterListener.java
@@ -3,7 +3,7 @@ package generator.cleaver;
 import generator.cleaver.model.RegistrationData;
 
 /**
- * Created by Prasanna Lakkur Subramanyam on 4/3/2016.
+ * Created by Sathish kumar on 4/3/2016.
  */
 public interface OnRegisterListener {
     String onRegister(RegistrationData data);
